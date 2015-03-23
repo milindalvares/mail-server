857c06c26a14c1d3bd397ed580ff97d613702144
milindalvares <milind@hashcooki.es>
2015-03-23 18:04:37 +0530

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "pony"
+gem "sinatra-cross_origin", "~> 0.3.1"
---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'rubygems'
+require 'pony'
+require 'sinatra/cross_origin'
+
+configure do
+	enable :cross_origin
+end
+
+get '/hi' do
+	erb :home
+end
+
+post '/contact' do
+	name = params[:name]
+	project_type = params[:project_type]
+	email = params[:email]
+	phone = params[:phone]
+	message = params[:message]
+	cm_original = params[:original]
+	Pony.mail(
+		:from => "#{name}<milind@hashcooki.es>",
+		:to => 'milind@hashcooki.es',
+		:subject => "Hash Cookies Request",
+		:html_body => "Project Pype: #{project_type}<br /> Message: #{message}<br /> Phone: #{phone}<br /> email: #{email}<br /> Original: #{cm_original}",
+		:via => :smtp,
+		:via_options => {
+			  :address              => 'smtp.sendgrid.net', 
+	        :port                 => '587', 
+	        :user_name            => '', 
+	        :password             => '', 
+	        :authentication       => :plain
+		}
+	)
+	return true
+end
